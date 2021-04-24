1f914ebf0768ac96a16983727bab5e556aa1ab84
Morgan Roberts <morgrob@email.unc.edu>
2021-04-24 02:41:51 -0400

    Styles and drag functions added to all pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,137 @@
-var drag = document.getElementById("dragtarget1");
-var drop = document.getElementById("droparea1");
+var drag1 = document.getElementById("dragtarget1");
+var drop1 = document.getElementById("droparea1");
 
 /* Events fired on the drag target */
 
-drag.addEventListener("dragstart", function(event) {
+drag1.addEventListener("dragstart", function(event) {
   event.dataTransfer.setData("Text", event.target.id);
 });
 
-drag.addEventListener("drag", function(event) {
-  document.getElementById("demo").innerHTML = "The p element is being dragged.";
-});
+// drag.addEventListener("drag", function(event) {
+//   document.getElementById("demo").innerHTML = "The p element is being dragged.";
+// });
 
 
 /* Events fired on the drop target */
-drop.addEventListener("dragover", function(event) {
+drop1.addEventListener("dragover", function(event) {
   event.preventDefault();
-  document.getElementById("demo").innerHTML = "The p element is OVER the droptarget.";
-  event.target.style.border = "4px dotted purple";
+//   document.getElementById("demo").innerHTML = "The p element is OVER the droptarget.";
+//   event.target.style.border = "4px dotted purple";
 });
 
-drop.addEventListener("drop", function(event) {
+drop1.addEventListener("drop", function(event) {
   event.preventDefault();
   var data = event.dataTransfer.getData("Text");
   event.target.appendChild(document.getElementById(data));
-  document.getElementById("demo").innerHTML = "The p element was dropped.";
+//   document.getElementById("demo").innerHTML = "The p element was dropped.";
 });
 
 document.getElementById("droparea1").ondrop = function() {
     location.href= "#second";
 };
+
+
+
+
+var drag2 = document.getElementById("dragtarget2");
+var drop2 = document.getElementById("droparea2");
+
+/* Events fired on the drag target */
+
+drag2.addEventListener("dragstart", function(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+});
+
+// drag.addEventListener("drag", function(event) {
+//   document.getElementById("demo").innerHTML = "The p element is being dragged.";
+// });
+
+
+/* Events fired on the drop target */
+drop2.addEventListener("dragover", function(event) {
+  event.preventDefault();
+//   document.getElementById("demo").innerHTML = "The p element is OVER the droptarget.";
+//   event.target.style.border = "4px dotted purple";
+});
+
+drop2.addEventListener("drop", function(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+//   document.getElementById("demo").innerHTML = "The p element was dropped.";
+});
+
+document.getElementById("droparea2").ondrop = function() {
+    location.href= "#third";
+    document.getElementById("virus-cluster").style.display = 'none';
+};
+
+
+
+var drag3 = document.getElementById("dragtarget3");
+var drop3 = document.getElementById("droparea3");
+
+/* Events fired on the drag target */
+
+drag3.addEventListener("dragstart", function(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+});
+
+// drag.addEventListener("drag", function(event) {
+//   document.getElementById("demo").innerHTML = "The p element is being dragged.";
+// });
+
+
+/* Events fired on the drop target */
+drop3.addEventListener("dragover", function(event) {
+  event.preventDefault();
+//   document.getElementById("demo").innerHTML = "The p element is OVER the droptarget.";
+//   event.target.style.border = "4px dotted purple";
+});
+
+drop3.addEventListener("drop", function(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+//   document.getElementById("demo").innerHTML = "The p element was dropped.";
+});
+
+document.getElementById("droparea3").ondrop = function() {
+    location.href= "#fourth";
+};
+
+
+var drag4 = document.getElementById("dragtarget4");
+var drop4 = document.getElementById("droparea4");
+
+/* Events fired on the drag target */
+
+drag4.addEventListener("dragstart", function(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+});
+
+// drag.addEventListener("drag", function(event) {
+//   document.getElementById("demo").innerHTML = "The p element is being dragged.";
+// });
+
+
+/* Events fired on the drop target */
+drop4.addEventListener("dragover", function(event) {
+  event.preventDefault();
+//   document.getElementById("demo").innerHTML = "The p element is OVER the droptarget.";
+//   event.target.style.border = "4px dotted purple";
+});
+
+drop4.addEventListener("drop", function(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+//   document.getElementById("demo").innerHTML = "The p element was dropped.";
+});
+
+document.getElementById("droparea4").ondrop = function() {
+    location.href= "#fifth";
+};
+
+
+
